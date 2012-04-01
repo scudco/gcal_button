00aa7d796ac3683b23fe31a23907f37740e9ff08
Adam Anderson <adamandersonis@gmail.com>
2012-03-31 18:37:09 -0700

    Add image URLs

--- lib/gcal_button.rb
+++ lib/gcal_button.rb
@@ -1,7 +1,63 @@
 require 'date'
 
 class GcalButton
-  def self.event_url(title, start_date, end_date, sprops)
-    "http://www.google.com/calendar/event?action=TEMPLATE&text;=My Event&dates;=20430426T180000Z/20430426T200000Z&sprop;=website:http://www.eventcompany.com"
+  GCAL_TIME_FORMAT = "%Y%m%dT%H%M%SZ"
+  GCAL_DATE_FORMAT = "%Y%m%d"
+
+  BASE_URL = "http://www.google.com/calendar/event"
+  IMAGE_URLS = {
+    default: "http://www.google.com/calendar/images/ext/gc_button1.gif",
+    remind_me: "http://www.google.com/calendar/images/ext/gc_button2.gif",
+    add_to: "http://www.google.com/calendar/images/ext/gc_button6.gif",
+    large: "http://www.google.com/calendar/images/ext/gc_button6.gif"
+  }
+
+  def initialize(title, start_time, end_time, sprops)
+    @title = title
+    @start_time = start_time
+    @end_time = end_time
+    @sprops = sprops
+  end
+
+  def url
+    "#{BASE_URL}?#{query_params}"
+  end
+
+  def image_url(type=:default)
+    IMAGE_URLS[type]
+  end
+
+  private
+  def sprops
+    @sprops.collect do |key,val|
+      ["sprop", "#{key}:#{val}"]
+    end
+  end
+
+  def dates
+    "#{formatted_time(@start_time)}/#{formatted_time(@end_time)}"
+  end
+
+  def formatted_time(time)
+    time.strftime(GCAL_TIME_FORMAT)
+  end
+
+  def query_params
+    params = {
+      action: "TEMPLATE",
+      dates: dates,
+      text: @title
+    }
+
+    params = params.to_a + sprops
+
+    params.collect do |key, value|
+      to_query(key, value)
+    end.sort * '&'
+  end
+
+  def to_query(key, value)
+    require 'cgi' unless defined?(CGI) && defined?(CGI::escape)
+    "#{CGI.escape(key.to_s)}=#{CGI.escape(value.to_s)}"
   end
 end
--- spec/gcal_button_spec.rb
+++ spec/gcal_button_spec.rb
@@ -1,21 +1,63 @@
 require 'gcal_button'
 
 describe GcalButton do
-  it "returns a Google Calendar Event URL" do
-    start_time = "April 26, 2043 6:00 PM"
-    end_time = "April 26, 2043 8:00 PM"
+  let (:start_time) { DateTime.parse("April 26, 2043 6:00 PM") }
+  let (:end_time) { DateTime.parse("April 26, 2043 8:00 PM") }
+  let (:button) do
+    GcalButton.new("My Event",
+                   start_time,
+                   end_time,
+                   website: "http://www.myeventcompany.com")
+  end
 
-    url = GcalButton.event_url("My Event",
-                               DateTime.parse(start_time),
-                               DateTime.parse(end_time),
-                               "http://myeventcompany.com")
+  describe "#url" do
+    it "returns a Google Calendar Event URL" do
+      expected_url = "http://www.google.com/calendar/event?" +
+        "action=TEMPLATE&" +
+        "dates=20430426T180000Z%2F20430426T200000Z&" +
+        "sprop=website%3Ahttp%3A%2F%2Fwww.myeventcompany.com&" +
+        "text=My+Event"
 
-    expected_url = "http://www.google.com/calendar/event?" +
-                    "action=TEMPLATE&" +
-                    "text;=My Event&" +
-                    "dates;=20430426T180000Z/20430426T200000Z&" +
-                    "sprop;=website:http://www.eventcompany.com"
+      button.url.should eql(expected_url)
+    end
 
-    url.should eql(expected_url)
+    it "can accept multiple sprop values" do
+      button = GcalButton.new("My Event",
+                              start_time,
+                              end_time,
+                              website: "http://www.myeventcompany.com",
+                              name: "My Event Company")
+
+      expected_url = "http://www.google.com/calendar/event?" +
+        "action=TEMPLATE&" +
+        "dates=20430426T180000Z%2F20430426T200000Z&" +
+        "sprop=name%3AMy+Event+Company&" +
+        "sprop=website%3Ahttp%3A%2F%2Fwww.myeventcompany.com&" +
+        "text=My+Event"
+
+      button.url.should eql(expected_url)
+    end
+  end
+
+  describe "#image_url" do
+    it "returns the URL to the 'Google Calendar' button image" do
+      url = "http://www.google.com/calendar/images/ext/gc_button1.gif"
+      button.image_url.should eql(url)
+    end
+
+    it "returns the URL to the 'Remind me with Google Calendar' button image" do
+      url = "http://www.google.com/calendar/images/ext/gc_button2.gif"
+      button.image_url(:remind_me).should eql(url)
+    end
+
+    it "returns the URL to the 'Add to Google Calendar' button image" do
+      url = "http://www.google.com/calendar/images/ext/gc_button6.gif"
+      button.image_url(:add_to).should eql(url)
+    end
+
+    it "returns the URL to a larger 'Google Calendar' button image" do
+      url = "http://www.google.com/calendar/images/ext/gc_button6.gif"
+      button.image_url(:large).should eql(url)
+    end
   end
 end
